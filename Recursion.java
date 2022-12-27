aed90b6844498e0bb1e320d5b3482930d8d8504d
Vaibhav <vaibhavverma.2106@gmail.com>
2022-12-27 19:07:10 +0530

    Done with Episode 29 'Sum of digits & power of a number using Recursion'

--- Recursion.java
+++ Recursion.java
@@ -25,6 +25,28 @@ public class Recursion {
     }
     // Finish Video 28
 
+    // Video 29
+    static int SumOfDigits(int n){
+//        if(n==0) return 0; // Adds another layer to the stack increasing space and time complexity
+        if(n>=0 && n<=9) return n;
+        return SumOfDigits(n/10) + n%10;
+    }
+    static int CountOfDigits(int n){
+        if(n>=0 && n<=9) return 1;
+        return CountOfDigits(n/10) + 1;
+    }
+    static int Pow(int p, int q){
+        if(q==0) return 1;
+        return Pow(p, q-1) * p;
+    }
+    static int Pow_Better(int p, int q){
+        if(q==0) return 1;
+        int half=Pow_Better(p, q/2);
+        if(q%2==0) return half*half;
+        return half*half*p;
+    }
+    // Finish Video 29
+
 
     public static void main(String[] args) {
 
